605cbfbd1fdb160f6f7d380c11d94d51d151277f
minzmann <minzmann@users.noreply.github.com>
2017-01-11 22:28:48 +0100

    Natürlich auf deutsch
    Courtesy of Google Translate and Bing

--- setup.py
+++ setup.py
@@ -1,74 +1,74 @@
-import codecs
-import os
-import re
+Importieren von Codecs
+Import-os
+Import
 
-from setuptools import setup, find_packages
+Von Setuptools importieren Setup, find_packages
 
 
-###################################################################
+################################################################################################### #################
 
-NAME = "pygflib"
-PACKAGES = find_packages(where="src")
-META_PATH = os.path.join("src", "pygflib", "__init__.py")
-KEYWORDS = ["gutefrage", "api", "rest", "gfapi"]
-CLASSIFIERS = [
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: MIT License",
-    "Natural Language :: English",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.5",
-    "Programming Language :: Python :: 3 :: Only",
-    "Topic :: Software Development :: Libraries :: Python Modules",
+NAME = "Pygflib"
+PAKETE = Find_packages (Wobei "Src" =)
+META_PATH = os.path.join ("Src", "Pygflib", "__init__.py")
+KEYWORDS = ["Gutefrage", "api", "Rest", "Gfapi"]
+KLASSIFIKATOREN =]
+"Entwicklungsstatus:: 3 - Alpha",
+"Zielgruppe: Entwickler",
+"Lizenzgebern:: OSI Genehmigt:: MIT Lizenzgebern",
+"Natürliche Sprache:: Englisch",
+"Betriebssystem: OS Unabhängig",
+"Programmiersprache:: Python",
+"Programmiersprache:: Python:: 3",
+"Programmiersprache:: Python:: 3,5",
+"Programmiersprache:: Python:: 3:: Nur",
+"Thema:: Software-Entwicklung:: Bibliotheken:: Python-Modul",
 ]
-INSTALL_REQUIRES = ["requests","lxml"]
+INSTALL_REQUIRES = ["Anfragen", "Lxml"]
 
-###################################################################
+################################################################################################### #################
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*parts):
-    """
-    Build an absolute path from *parts* and and return the contents of the
-    resulting file.  Assume UTF-8 encoding.
-    """
-    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
-        return f.read()
+HIER = os.path.abspath (os.path.dirname (__ File__))
 
 
-META_FILE = read(META_PATH)
+DEF Lesen (* Teile):
+Aufrechtzuerhalten.
+Erstellen Sie Einen Absoluten Biodiesel aus * Teile * Und Geben Sie Den Inhalt der
+Resultierende Datei. Nehmen Wir UTF-8-Codierung ein.
+Aufrechtzuerhalten.
+Mit codecs.open (os.path.join (HIER * Teile), "Rb", "Utf-8") als f:
+Rückkehr f.read)
 
 
-def find_meta(meta):
-    """
-    Extract __*meta*__ from META_FILE.
-    """
-    meta_match = re.search(
-        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
-        META_FILE, re.M
-    )
-    if meta_match:
-        return meta_match.group(1)
-    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+META_FILE = Read (META_PATH)
 
 
-if __name__ == "__main__":
-    setup(
-        name=NAME,
-        description=find_meta("description"),
-        license=find_meta("license"),
-        version=find_meta("version"),
-        author=find_meta("author"),
-        maintainer=find_meta("author"),
-        keywords=KEYWORDS,
-        url="https://github.com/waz4bb/pygflib",
-        long_description=read("README.md"),
-        packages=PACKAGES,
-        package_dir={"": "src"},
-        zip_safe=False,
-        classifiers=CLASSIFIERS,
-        install_requires=INSTALL_REQUIRES,
-    )
+DEF Find_meta (Meta):
+Aufrechtzuerhalten.
+Aus __ * Meta * __ aus META_FILE Extrahieren.
+Aufrechtzuerhalten.
+Meta_match = Suche (
+(Meta = Meta), Wobei Das Format (Meta Meta =)
+META_FILE, Re. M
+)
+Wenn Meta_match:
+Return meta_match.group (1)
+RuntimeError ("Unable to __ __ {Meta} Zeichenfolge zu finden." Formatieren (Meta Meta =))
+
+
+Wenn __name__ == "__main__":
+Konfiguration)
+Name = NAME,
+Beschreibung = Find_meta ("Beschreibung"),
+Lizenzgebern = Find_meta ("nun"),
+Version = Find_meta ("Version")
+Autor = Find_meta ("Autor"),
+Betreuer = Find_meta ("Autor"),
+Keywords = SCHLÜSSELWÖRTER,
+URL = "https://github.com/waz4bb/pygflib"
+Long_description = Lesen ("README.md"),
+Verwöhnpaketes = VERWÖHNPAKETES,
+Package_dir = {"": "Src"},
+Zip_safe = False,
+Klassifikatoren = KLASSIFIKATOREN,
+Install_requires = INSTALL_REQUIRES,
+)
